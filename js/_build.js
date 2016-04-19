5d665ce34634d26cfa1d2483d4f50b68f245a944
unknown <TomHinchliffe@AEROXCRAFT.local>
2016-04-19 11:40:05 +0100

    Cleaning up code

--- js/_build.js
+++ js/_build.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 'use strict';
 
 (function (window, document) {
@@ -21,3 +22,28 @@
   }
 
 }(window, document));
+=======
+'use strict';
+
+(function (window, document) {
+
+  //import("pathformer.js");
+  //import("vivus.js");
+
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], function() {
+      return Vivus;
+    });
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = Vivus;
+  } else {
+    // Browser globals
+    window.Vivus = Vivus;
+  }
+
+}(window, document));
+>>>>>>> 1c352534071be6cba5600987b43e4ed1e7d08303
